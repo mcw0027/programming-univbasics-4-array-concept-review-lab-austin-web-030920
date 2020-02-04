7e2b8525018bcd3c5ad69a6e8564e9d88db912d6
Michael Wixson <wixsonmc@gmail.com>
2020-02-04 21:44:20 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
-  array.length.times do {
-    if array[counter] == value_to_find
+  array.length.times do |count|
+    if array[count] == value_to_find
       return array.[counter]
     else return nil
   }
